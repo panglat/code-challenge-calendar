499e26f79ed0535b8e49cdd7891d79dca80c7de8
Pablo Anglat <pablo.anglat@globant.com>
2020-03-28 20:15:29 -0300

    Styling

--- src/components/CalendarDay/index.js
+++ src/components/CalendarDay/index.js
@@ -9,14 +9,21 @@ import './styles.scss';
 const CalendarDay = ({ className, date, monthNumber, ...rest }) => {
   return (
     <div className={cn('calendar-day', className)} {...rest}>
-      {date.date()}
+      <div
+        className={cn('calendar-day__day', {
+          'calendar-day__day--other-month': date.months() + 1 !== monthNumber,
+        })}
+      >
+        {date.date()}
+      </div>
     </div>
   );
 };
 
 CalendarDay.propTypes = {
   className: PropTypes.string,
-  date: PropTypes.shape({ date: PropTypes.func }).isRequired,
+  date: PropTypes.shape({ date: PropTypes.func, months: PropTypes.func })
+    .isRequired,
   monthNumber: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
     .isRequired,
 };
