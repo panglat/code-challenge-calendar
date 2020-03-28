ae826300aa38f613c3df945ae498ab2aaa1273a2
Pablo Anglat <pablo.anglat@globant.com>
2020-03-27 21:56:29 -0300

    Add AirBnB’s ESLint config and Prettier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
