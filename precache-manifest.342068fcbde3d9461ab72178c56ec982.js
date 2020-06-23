3b28f03c874945e9c56ebb188f95bbf093155826
panglat <panglat@gmail.com>
2020-06-23 04:18:17 +0000

    Updates

---
+++ precache-manifest.342068fcbde3d9461ab72178c56ec982.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26a18c24d0ba0cdefc5e4f39481a1ae",
+    "url": "/code-challenge-calendar/index.html"
+  },
+  {
+    "revision": "c10f1a24419c016dfaea",
+    "url": "/code-challenge-calendar/static/css/2.daed29bd.chunk.css"
+  },
+  {
+    "revision": "4ce64b1727329fff7d91",
+    "url": "/code-challenge-calendar/static/css/main.0172e937.chunk.css"
+  },
+  {
+    "revision": "c10f1a24419c016dfaea",
+    "url": "/code-challenge-calendar/static/js/2.07ef0e8d.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/code-challenge-calendar/static/js/2.07ef0e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce64b1727329fff7d91",
+    "url": "/code-challenge-calendar/static/js/main.9ff3f4f3.chunk.js"
+  },
+  {
+    "revision": "b063adf309a78bc86ad6",
+    "url": "/code-challenge-calendar/static/js/runtime-main.b9655539.js"
+  }
+]);
